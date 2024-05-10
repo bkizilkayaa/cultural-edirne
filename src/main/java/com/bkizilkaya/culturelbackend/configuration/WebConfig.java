ea67242c1a7a4ed54e463d055b14cb0b69c628aa
Burak Kizilkaya <burakkizilkaya@trakya.edu.tr>
2024-05-10 21:14:20 +0300

    cors ayarlamalari yapiliyor

--- src/main/java/com/bkizilkaya/culturelbackend/configuration/WebConfig.java
+++ src/main/java/com/bkizilkaya/culturelbackend/configuration/WebConfig.java
@@ -11,6 +11,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/javaconfig").allowedOrigins("http://localhost:8080");
+        registry.addMapping("/**").allowedOrigins("http://localhost:8080");
     }
 }
