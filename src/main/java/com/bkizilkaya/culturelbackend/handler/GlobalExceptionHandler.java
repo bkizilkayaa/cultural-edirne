5550d583b8dbce884f24bf41e2e90d46f66120e9
Burak Kizilkaya <burakkizilkaya@trakya.edu.tr>
2024-05-23 03:38:33 +0300

    exception durumlarinda artik uygulama seviyesinde log basiliyor.

--- src/main/java/com/bkizilkaya/culturelbackend/handler/GlobalExceptionHandler.java
+++ src/main/java/com/bkizilkaya/culturelbackend/handler/GlobalExceptionHandler.java
@@ -4,17 +4,19 @@ import com.bkizilkaya.culturelbackend.exception.CustomErrorResponse;
 import com.bkizilkaya.culturelbackend.exception.NotFoundException;
 import com.bkizilkaya.culturelbackend.exception.ValidationException;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
-@RestControllerAdvice
+@ControllerAdvice
+@Log4j2
 public class GlobalExceptionHandler {
     private final HttpServletRequest request;
 
@@ -25,12 +27,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = {ValidationException.class})
     public ResponseEntity<CustomErrorResponse> handleCustomException(ValidationException ex) {
         CustomErrorResponse errorResponse = customErrorResponseBuilder(BAD_REQUEST, ex.getMessage());
+        log.error("Validation error occurred! :>> " + ex.getMessage());
         return new ResponseEntity<>(errorResponse, BAD_REQUEST);
     }
 
     @ExceptionHandler(value = {NotFoundException.class})
     public ResponseEntity<CustomErrorResponse> handleCustomException(NotFoundException ex) {
         CustomErrorResponse errorResponse = customErrorResponseBuilder(NOT_FOUND, ex.getMessage());
+        log.error(ex.getObjectType() + " not found!");
         return new ResponseEntity<>(errorResponse, NOT_FOUND);
     }
 
